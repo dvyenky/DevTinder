d4d3eca01ed3aa047be188cc443a7f7bc9828cba
dvyenky <dvyenkatesh@gmail.com>
2026-03-07 12:45:55 +0530

    Explore the more API and Work on HTTP Methods

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # DevTinder
+
+app.use("/endPoint",(req, res) => {
+    // these is known as route handler
+})
+
+
+http methods in RestAPI 
+get, post, put, patch, delete 
+
--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.use("/test", (req, res) => {
   res.json("Hello From test EndPoint");
 });
 
+app.use("/hello/abc", (req, res) => {
+  res.send("Hello World from Another hello Routing");
+});
+
 app.use("/hello", (req, res) => {
   res.json("Hello From hello EndPoint");
 });
@@ -16,6 +20,24 @@ app.use("/getDetails", (req, res) => {
     name: "vyenkatesh",
     city: "pune",
   });
+});
+
+app.get("/user/:id", (req, res) => {
+  console.log(req.params);
+  res.send("User fetch the data : ", req.params);
+});
+
+
+app.get("/user", (req, res) => {
+  res.send("Fetch the User Data Successfully");
+});
+
+app.post("/user", (req, res) => {
+  res.send("Save the User Successfully");
+});
+
+app.use("/", (req, res) => {
+  res.send("Hello Server");
 });
 
 app.listen(port, () => {
