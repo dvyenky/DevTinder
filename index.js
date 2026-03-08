cb6caeb5bc96529e39b42b4a71f4f35c621689f9
dvyenky <dvyenkatesh@gmail.com>
2026-03-08 13:18:40 +0530

    How query string and params work in URL

--- index.js
+++ index.js
@@ -3,41 +3,15 @@ const port = 7777;
 
 const app = express();
 
-app.use("/test", (req, res) => {
-  res.json("Hello From test EndPoint");
+app.get("/user/:userId", (req, res) => {
+  const req_id = req.params.userId;
+  res.send(`your userid =>  ${req_id}`);
 });
 
-app.use("/hello/abc", (req, res) => {
-  res.send("Hello World from Another hello Routing");
-});
-
-app.use("/hello", (req, res) => {
-  res.json("Hello From hello EndPoint");
-});
-
-app.use("/getDetails", (req, res) => {
-  res.json({
-    name: "vyenkatesh",
-    city: "pune",
-  });
-});
-
-app.get("/user/:id", (req, res) => {
-  console.log(req.params);
-  res.send("User fetch the data : ", req.params);
-});
-
-
-app.get("/user", (req, res) => {
-  res.send("Fetch the User Data Successfully");
-});
-
-app.post("/user", (req, res) => {
-  res.send("Save the User Successfully");
-});
-
-app.use("/", (req, res) => {
-  res.send("Hello Server");
+app.get("/getUserDetails", (req, res) => {
+  const city = req.query.city;
+  const name = req.query.name;
+  res.send(`The ${name} is belong to ${city}`);
 });
 
 app.listen(port, () => {
